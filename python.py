3d564951fd512e4c9e6a30886db7abc991f8e81a
Mohammad Kazim <120630543+Kazim424@users.noreply.github.com>
2023-10-18 14:36:19 -0400

    first python hello world program
    first python hello world program

---
+++ python.py
@@ -0,0 +1,34 @@
+# WAP program to print day name of week
+
+# Input week number from user 
+
+weekDayNumber=int(input("Enter week number (1-7): "))
+if(weekDayNumber == 1):
+    print("Monday")   
+elif(weekDayNumber == 2):   
+    print("Tuesday")
+    
+elif(weekDayNumber == 3):
+    
+    print("Wednesday")
+    
+elif(weekDayNumber == 4):
+    
+    print("Thursday")
+    
+elif(weekDayNumber == 5):
+    
+    print("Friday")
+    
+elif(weekDayNumber == 6):
+    
+    print("Saturday")
+    
+elif(weekDayNumber == 7):
+    
+    print("Sunday")
+    
+else:
+    
+    print("Invalid Input! Please enter week number between 1-7.")
+    
