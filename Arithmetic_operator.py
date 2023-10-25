22013535147991828487e03b5597726edc069730
Mohammad Kazim <120630543+Kazim424@users.noreply.github.com>
2023-10-25 06:01:56 -0400

    Program to perform arithmetic operator
    All arithmetic operator program perform

---
+++ Arithmetic_operator.py
@@ -0,0 +1,19 @@
+#  python program to perform all arithmetic operations
+
+num1=15
+num2 =2
+
+sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+modu=num1%num2
+power=num1**num2
+FloorDivision=num1//num2
+print("Addition of given number is: " ,sum)
+print("Subtraction of given number is: " ,sub)
+print("Multiplication of given number is: " ,mul)
+print("Division of given number is: " ,div)
+print("Remainder given after division of two number is: " ,modu)
+print("Power of given number is; " ,power) #exponentional
+print("Floor division of given number is: " ,FloorDivision)
