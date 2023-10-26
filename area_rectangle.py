61fd4571918f684601b5eb188ad29b0ddedb9b66
Mohammad Kazim <120630543+Kazim424@users.noreply.github.com>
2023-10-26 08:37:10 -0400

    area of rectangle program
    Area of rectangle program

---
+++ area_rectangle.py
@@ -0,0 +1,10 @@
+#  python program to find area and perimeter of rectangle
+
+length=float(input("Enter the length of rectangle "))
+breadth=float(input("Enter the width of rectangle "))
+
+perimeter=length+breadth
+area =length*breadth
+
+print("Area of rectangle is: ",area)
+print("Perimeter of rectangle is: ",perimeter)
