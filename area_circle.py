a5c85583d0f153684bbed9ab41cdb12b9210f24f
Mohammad Kazim <120630543+Kazim424@users.noreply.github.com>
2023-10-26 08:38:06 -0400

    Area of circle program
    To find area of circle program

---
+++ area_circle.py
@@ -0,0 +1,9 @@
+# WAP to print area and circumprance and diameter of  circle
+
+radius=float(input("Enter the radius of circle "))
+diameter = 2 * radius
+circumference = 2 * 3.14 * radius
+area = 3.14 * (radius * radius)
+print("The diameter of circle is ",diameter)
+print("The circumference of circle is ",circumference)
+print("The area of circle is ",area)
