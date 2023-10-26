40308a30d0029b39583eaab92b194a081b48ac2a
Mohammad Kazim <120630543+Kazim424@users.noreply.github.com>
2023-10-26 08:38:52 -0400

    To find area of triangle program
    To find area of triangle program in python

---
+++ area_triangle.py
@@ -0,0 +1,8 @@
+# WAP  to print the area of triangle 
+
+base= float(input("Enter the base of triangle is: "))
+height= float(input("Enter the height of triangle is: "))
+
+#  Calculate area of triangle 
+area = (base * height) / 2
+print("The area of triangle is = ",area)
